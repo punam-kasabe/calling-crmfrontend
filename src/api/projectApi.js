73dcb4adc6398899ba94d18f026d94ec2f4cbd20
Punam Kasabe <punamkasabe21@gmail.com>
2026-05-15 11:34:29 +0530

    updated backend url

--- src/api/projectApi.js
+++ src/api/projectApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://calling-crm-backend-7w52.onrender.com";
+const BASE_URL = "https://calling-crm-backend-7w52.onrender.com/api";
+
 // GET all projects
 export const getProjects = async () => {
   const res = await axios.get(`${BASE_URL}/projects`);
