0920510c4848894bedf6638459e8a0ec65f39a07
Punam Kasabe <punamkasabe21@gmail.com>
2026-05-23 13:20:29 +0530

    search box

--- src/pages/executive/MyLeads.js
+++ src/pages/executive/MyLeads.js
@@ -720,36 +720,21 @@ return (
       <div className="filter-bar">
      
 
-        {/* ================= SEARCH BOX ================= */}
+         {/* ================= SINGLE SEARCH ================= */}
 
-<div className="search-wrapper">
+         <div className="filter-bar">
 
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="search-icon"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M21 21l-4.35-4.35m1.85-5.15a7 7 0 11-14 0 7 7 0 0114 0z"
-    />
-  </svg>
+        <input
+         type="text"
+         placeholder="Type to search"
+         value={search}
+         onChange={(e) =>
+        setSearch(e.target.value)
+       }
+         className="global-search"
+       />
 
-  <input
-    type="text"
-    placeholder="Type to search"
-    value={search}
-    onChange={(e) =>
-      setSearch(e.target.value)
-    }
-    className="search-input"
-  />
-
-</div>
+       </div>
        </div>
 {/* ================= ACTION BUTTONS ================= */}
 
