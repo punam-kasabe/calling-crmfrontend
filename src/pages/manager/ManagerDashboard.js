2d358db65c4e56a55ab510972b6d94bfdf50b695
Punam Kasabe <punamkasabe21@gmail.com>
2026-06-26 13:29:19 +0530

    Added attending officer dropdown updates

--- src/pages/manager/ManagerDashboard.js
+++ src/pages/manager/ManagerDashboard.js
@@ -76,7 +76,7 @@ const [projectStats, setProjectStats] =
         {
           params: {
             email: user.email,
-            role: "manager"
+            role: "Sales"
           }
         }
       );
